2abb8034c5e857157fc25aa253234cc9a3f3a6de
AbdelRahman Atef Mostafa <abdelrahman.atef.712@gmail.com>
2022-11-17 11:56:31 +0200

    Update main.c

--- ADC_TEMPERATURE_SENSOR_LM35/APP/main.c
+++ ADC_TEMPERATURE_SENSOR_LM35/APP/main.c
@@ -1,4 +1,4 @@
-/*
+
  * main.c
  *
  *  Created on: Sep 15, 2022
@@ -6,7 +6,7 @@
  */
 
 
-///************************* Application on Interrupts using INT0 ********************//
+///************************* Application on ADC using Temperature sensor LM35 ********************//
 
 
 ///*****************INCLUDES**********//
@@ -16,28 +16,21 @@
 
 int main ()
 {
-	H_LCD_Void_LCDInt();
-	M_ADC_void_AdcInt();
-	H_TEMPSENSOR_void_TempSensorInt();
-
-   u8 x= 0;
+	H_LCD_Void_LCDInt();                  ///// LCD initialization 
+	M_ADC_void_AdcInt();                 ///// ADC initialization 
+	H_TEMPSENSOR_void_TempSensorInt();  ///// Temperature sensor initialization 
+  
+   u8 Local_u8SensorValue= 0;
 
     while (1)
        {
-    	 x=  H_TEMPSENSOR_void_TempSensorRead();
-
+    	 Local_u8SensorValue = H_TEMPSENSOR_void_TempSensorRead();
           _delay_ms(1000);
-         H_Lcd_void_LCDWriteNum(x);
+         H_Lcd_void_LCDWriteNum(Local_u8SensorValue);
           _delay_ms(1000);
          H_Lcd_void_LCDClear();
 
        }
 
 
-}
-
-void A_TIMER0_void_Timer0Exc(void)
-{
-   H_LED_Void_LedOn(BLUE_LED);
-   H_LED_Void_LedTogg(BLUE_LED);
 }
