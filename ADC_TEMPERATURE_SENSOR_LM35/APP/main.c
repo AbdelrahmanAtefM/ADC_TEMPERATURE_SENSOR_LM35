2b85b651c9d36e41df88e1d9bbcbf0028bfdb1e0
AbdelRahman Atef Mostafa <abdelrahman.atef.712@gmail.com>
2022-11-17 12:27:36 +0200

    Update main.c

--- ADC_TEMPERATURE_SENSOR_LM35/APP/main.c
+++ ADC_TEMPERATURE_SENSOR_LM35/APP/main.c
@@ -1,4 +1,4 @@
-
+*
  * main.c
  *
  *  Created on: Sep 15, 2022
@@ -16,19 +16,21 @@
 
 int main ()
 {
-	H_LCD_Void_LCDInt();                  ///// LCD initialization 
-	M_ADC_void_AdcInt();                 ///// ADC initialization 
-	H_TEMPSENSOR_void_TempSensorInt();  ///// Temperature sensor initialization 
-  
-   u8 Local_u8SensorValue= 0;
+	H_LCD_Void_LCDInt();                  ///// LCD initialization
+	M_ADC_void_AdcInt();                 ///// ADC initialization
+	H_TEMPSENSOR_void_TempSensorInt();  ///// Temperature sensor initialization
+
+   u8 Local_u8SensorValue= 0;         /// local variable to take sensor value
 
     while (1)
        {
+    	/////taking sensor value
     	 Local_u8SensorValue = H_TEMPSENSOR_void_TempSensorRead();
-          _delay_ms(1000);
+          _delay_ms(1000);   ////delay for 1 sec
+        //////printing the value on the LCD
          H_Lcd_void_LCDWriteNum(Local_u8SensorValue);
-          _delay_ms(1000);
-         H_Lcd_void_LCDClear();
+          _delay_ms(1000); ////delay for 1 sec
+         H_Lcd_void_LCDClear(); /// clear screen
 
        }
 
